7d32eb884c43823d660f6d0d9f0f0bc36afd8e57
Rasyid Syahadah <50646542+syahadae@users.noreply.github.com>
2026-01-26 17:19:51 +0700

    Toggle ExtremeFpsBoost and UltimatePerformance settings

--- FishItConfig.lua
+++ FishItConfig.lua
@@ -142,8 +142,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Endgame"] = "",
     },
 
-    ["ExtremeFpsBoost"] = false,
-    ["UltimatePerformance"] = true,
+    ["ExtremeFpsBoost"] = true,
+    ["UltimatePerformance"] = false,
     ["Disable3DRender"] = false,
     ["AutoRemovePlayer"] = true,
 
